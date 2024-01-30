5906c54317f0cab690d8c456fc0186d2f90222c1
Chinmaya0011 <103204520+Chinmaya0011@users.noreply.github.com>
2024-01-31 00:30:14 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../Styles/About.css'; // Import the CSS file for styling
+import '../Styles/about.css'; // Import the CSS file for styling
 
 function About() {
   return (
